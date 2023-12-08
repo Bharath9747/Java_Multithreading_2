f8f3e8984a7520de18db381e63c0ddfccde379da
Bharath9747 <152379838+Bharath9747@users.noreply.github.com>
2023-12-08 22:46:14 +0530

    Add files via upload

--- QuickSort.java
+++ QuickSort.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class QuickSort {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ArrayList<Integer> arr = new ArrayList<>();
+        List<Integer> arr = new ArrayList<>();
         arr.add(10);
         arr.add(2);
         arr.add(23);
@@ -13,9 +14,9 @@ public class QuickSort {
 
         ExecutorService executorService = Executors.newFixedThreadPool(2);
         QuickSortCal quickSortCal = new QuickSortCal(arr);
-        Future<ArrayList<Integer>> listFuture = executorService.submit(quickSortCal);
-        ArrayList<Integer> res = listFuture.get();
-        System.out.println(quickSortCal.arr);
+        Future<List<Integer>> listFuture = executorService.submit(quickSortCal);
+        List<Integer> res = listFuture.get();
+        System.out.println(res);
         executorService.shutdown();
     }
 }
--- QuickSortCal.java
+++ QuickSortCal.java
@@ -1,34 +1,53 @@
 import java.util.ArrayList;
-import java.util.concurrent.Callable;
+import java.util.List;
+import java.util.concurrent.*;
 
-public class QuickSortCal implements Callable{
-    ArrayList<Integer> arr;
-    public QuickSortCal(ArrayList<Integer> arr) {
+public class QuickSortCal implements Callable<List<Integer>>{
+    List<Integer> arr;
+    public QuickSortCal(List<Integer> arr) {
         this.arr = arr;
     }
 
     @Override
-    public Object call() throws Exception {
-         quicksort(0,arr.size()-1,arr);
-        return null;
+    public List<Integer> call() throws Exception {
+         quicksort(arr);
+        return arr;
     }
 
-    private void quicksort(int i, int i1, ArrayList<Integer> arr) {
-        if (i < i1) {
-            int pi = partition(arr, i, i1);
-            quicksort( i, pi - 1,arr);
-            quicksort( pi + 1, i1,arr);
+    private void quicksort( List<Integer> arr) throws ExecutionException, InterruptedException {
+        if (arr.size() > 1) {
+            int pi = arr.get(0);
+            List<Integer> leftList = new ArrayList<>();
+            List<Integer> rightList = new ArrayList<>();
+            for(int i=1;i<arr.size();i++)
+            {
+                if(arr.get(i)<=pi)
+                    leftList.add(arr.get(i));
+                else
+                    rightList.add(arr.get(i));
+            }
+
+            ExecutorService executorService = Executors.newFixedThreadPool(2);
+            Future<List<Integer>> leftpartition = executorService.submit(new QuickSortCal(leftList));
+            Future<List<Integer>> rightpartition = executorService.submit(new QuickSortCal(rightList));
+            List<Integer> lr = leftpartition.get();
+            List<Integer> rr = rightpartition.get();
+            arr.clear();
+            arr.addAll(lr);
+            arr.add(pi);
+            arr.addAll(rr);
+            executorService.shutdown();
         }
     }
 
 
-    static void swap(ArrayList<Integer> arr, int i, int j)
+    static void swap(List<Integer> arr, int i, int j)
     {
         int temp = arr.get(i);
         arr.set(i,j);
         arr.set(j,temp);
     }
-    static int partition(ArrayList<Integer> arr, int low, int high)
+    static int partition(List<Integer> arr, int low, int high)
     {
         int pivot = arr.get(high);
         int i = (low - 1);
